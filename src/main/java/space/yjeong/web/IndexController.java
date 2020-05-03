d3d7856e4a09cccaf181ae4a1e98a6a5a122204d
YJeongKim <yjeong20s@naver.com>
2020-05-03 14:29:32 +0900

    :sparkles: Create IndexControllerTest

--- src/main/java/space/yjeong/web/IndexController.java
+++ src/main/java/space/yjeong/web/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String indxe() {
+    public String index() {
         return "index";
     }
 }
