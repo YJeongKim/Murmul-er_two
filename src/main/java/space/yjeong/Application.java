a7fd84da7554dd878ae98c875121ea8bc6ebea7b
YJeongKim <yjeong20s@naver.com>
2020-05-14 17:34:26 +0900

    :sparkles: Create JpaConfig, SwaggerConfig

--- src/main/java/space/yjeong/Application.java
+++ src/main/java/space/yjeong/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  /* JPA Auditing 활성화 */
 @SpringBootApplication
 public class Application {  /* 메인 클래스 */
     public static void main(String[] args) {
