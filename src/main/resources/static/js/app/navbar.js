2e141d1a5c83be0d84418fd056808d39750cdc7c
YJeongKim <yjeong20s@naver.com>
2020-07-14 00:02:24 +0900

    :tada: Add navbar click function

---
+++ src/main/resources/static/js/app/navbar.js
@@ -0,0 +1,49 @@
+var navbar = {
+    isLogin : 0,
+    init : function () {
+        let _this = this;
+
+        _this.isLogin = $(".navbar-buttons").find('ul').length;
+
+        $('#nav-search').on('click', function () {
+            _this.moveBeforeLogin('/');
+        });
+        $('#nav-register').on('click', function () {
+            _this.moveAfterLogin('/rooms/register');
+        });
+        $('#nav-manage').on('click', function () {
+            _this.moveAfterLogin('/rooms');
+        });
+    }, moveAfterLogin : function (location) {
+        if(this.isLogin === 1) {
+            window.location.href = location;
+        } else {
+            swalAlert('warning', '로그인하면 이용할 수 있습니다.');
+        }
+    }, moveBeforeLogin : function (location) {
+        window.location.href = location;
+    }
+}
+
+navbar.init();
+
+$(document).ready(function () {
+    $('#logout').on('click', function () {
+        swalAlert('info', '로그아웃을 진행합니다.');
+        location.href = '/logout';
+    });
+    $('#login').on('click', function () {
+        swalAlert('info', '로그인을 진행합니다.');
+        location.href = '/oauth2/authorization/google';
+    });
+});
+
+var swalAlert = function (type, title) {
+    Swal.fire({
+        position: 'center',
+        type: type,
+        title: title,
+        showConfirmButton: false,
+        timer: 1500
+    });
+}
