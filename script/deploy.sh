f9912a93e0e8810f9509aecd41be3b4f95ee0f96
YJeongKim <yjeongisme@gmail.com>
2020-11-07 22:28:11 +0900

    :firecracker: Modify deploy script about copying jar path

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=Murmul-er
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/build/build/libs/*.jar $REPOSITORY/jar/
+cp $REPOSITORY/build/*.jar $REPOSITORY/jar/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
