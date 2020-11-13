ac77a0dd178b7ce58a199c7fa0cb2b5d49287025
YJeongKim <yjeongisme@gmail.com>
2020-11-13 21:18:34 +0900

    :tada: Add web config about mustache

--- src/main/java/space/yjeong/config/WebConfig.java
+++ src/main/java/space/yjeong/config/WebConfig.java
@@ -1,8 +1,14 @@
 package space.yjeong.config;
 
+import com.samskivert.mustache.Mustache.TemplateLoader;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    private static TemplateLoader mustacheTemplateLoader() {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        String prefix = "classpath:/templates/";
+        Charset charset = Charset.forName("UTF-8");
+        return name -> new InputStreamReader(
+                resourceLoader.getResource(prefix + name).getInputStream(), charset);
     }
 }
